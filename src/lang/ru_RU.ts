fb25ab916bf5624419ee6eb43c78aa85008d956f
Jura Prokofev <Smyrke2005@yandex.ru>
2021-05-05 22:40:25 +0300

    Ru Translate update

--- src/lang/ru_RU.ts
+++ src/lang/ru_RU.ts
@@ -4,12 +4,12 @@
 <context>
     <name>ConsoleWidget</name>
     <message>
-        <location filename="../consoleWidget.cpp" line="237"/>
+        <location filename="../consoleWidget.cpp" line="253"/>
         <source>terminal</source>
         <translation>терминал</translation>
     </message>
     <message>
-        <location filename="../consoleWidget.cpp" line="241"/>
+        <location filename="../consoleWidget.cpp" line="257"/>
         <source>console</source>
         <translation>консоль</translation>
     </message>
@@ -22,151 +22,204 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="25"/>
+        <location filename="../mainwindow.ui" line="126"/>
         <source>2400</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="30"/>
+        <location filename="../mainwindow.ui" line="131"/>
         <source>9600</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="35"/>
+        <location filename="../mainwindow.ui" line="136"/>
         <source>19200</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="40"/>
+        <location filename="../mainwindow.ui" line="141"/>
         <source>115200</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="52"/>
-        <source>10</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="57"/>
-        <source>20</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="62"/>
-        <source>50</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="67"/>
-        <source>100</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="72"/>
-        <source>200</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="77"/>
-        <source>500</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="82"/>
-        <source>1000</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="104"/>
+        <location filename="../mainwindow.ui" line="37"/>
         <source>CLEAR</source>
         <translation>Очистить</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="154"/>
+        <location filename="../mainwindow.ui" line="79"/>
         <source>&lt;b&gt;Speed:&lt;/b&gt;</source>
         <translation>&lt;b&gt;Скорость:&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="168"/>
+        <location filename="../mainwindow.ui" line="51"/>
         <source>&lt;b&gt;Timeout:&lt;/b&gt;</source>
         <translation>&lt;b&gt;Таймаут:&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="195"/>
+        <location filename="../mainwindow.ui" line="169"/>
         <source>File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="201"/>
+        <location filename="../mainwindow.ui" line="175"/>
         <source>Modbus</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="208"/>
+        <location filename="../mainwindow.ui" line="183"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="218"/>
+        <location filename="../mainwindow.ui" line="194"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="226"/>
+        <location filename="../mainwindow.ui" line="202"/>
         <source>Search for packages</source>
+        <translation>Поиск пакетов</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="207"/>
+        <source>Calculate RTU CRC</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="231"/>
-        <source>Calculate Modbus RTU CRC</source>
+        <location filename="../mainwindow.ui" line="215"/>
+        <source>HEX Data at new line</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="118"/>
+        <location filename="../mainwindow.ui" line="220"/>
+        <source>Packet Creator</source>
+        <translation>Генератор пакетов</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="93"/>
         <source>MODE</source>
         <translation>РЕЖИМ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="90"/>
+        <location filename="../mainwindow.ui" line="86"/>
         <source>&lt;b&gt;ms&lt;/b&gt;</source>
         <translation>&lt;b&gt;мс&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="97"/>
+        <location filename="../mainwindow.ui" line="44"/>
         <source>&lt;b&gt;Port:&lt;/b&gt;</source>
         <translation>&lt;b&gt;Порт:&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="141"/>
-        <location filename="../mainwindow.cpp" line="34"/>
-        <location filename="../mainwindow.cpp" line="58"/>
+        <location filename="../mainwindow.ui" line="21"/>
+        <location filename="../mainwindow.cpp" line="40"/>
+        <location filename="../mainwindow.cpp" line="65"/>
         <source>OPEN</source>
         <translation>ОТКРЫТЬ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="131"/>
+        <location filename="../mainwindow.ui" line="149"/>
         <source>HEX</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="47"/>
+        <location filename="../mainwindow.cpp" line="54"/>
         <source>CLOSE</source>
         <translation>ЗАКРЫТЬ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="125"/>
+        <location filename="../mainwindow.cpp" line="145"/>
         <source>Mode: %1</source>
         <translation>Режим: %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="274"/>
+        <location filename="../mainwindow.cpp" line="314"/>
         <source>Console</source>
         <translation>Консоль</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="277"/>
+        <location filename="../mainwindow.cpp" line="318"/>
         <source>Terminal</source>
         <translation>Терминал</translation>
+    </message>
+</context>
+<context>
+    <name>ModbusPacketCreator</name>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="14"/>
+        <source>Modbus Packet Creator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="20"/>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="27"/>
+        <source>Type:</source>
+        <translation>Тип:</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="33"/>
+        <source>RTU</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="66"/>
+        <source>&lt;b&gt;CRC:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="83"/>
+        <source>Data Direction: </source>
+        <translation>Направление:</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="89"/>
+        <source>Request</source>
+        <translation>Запрос</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="99"/>
+        <source>Response</source>
+        <translation>Ответ</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="109"/>
+        <source>&lt;b&gt;Code:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Код:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="116"/>
+        <source>&lt;b&gt;ID:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="123"/>
+        <source>HH HH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="133"/>
+        <source>&lt;b&gt;Count:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Кол-во:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="147"/>
+        <source>&lt;b&gt;Addr:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.ui" line="161"/>
+        <source>Request add data size</source>
+        <translation>Добавление размера данных в запросе</translation>
+    </message>
+    <message>
+        <location filename="../windows/modbuspacketcreator.cpp" line="15"/>
+        <source>Value</source>
+        <translation>Значение</translation>
     </message>
 </context>
 </TS>
